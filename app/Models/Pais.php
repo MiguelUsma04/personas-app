fc5ba36371ac951446b942857373a14dd71fd94f
Miguel Usma <usmamiguel04@gmail.com>
2023-10-20 20:30:15 -0500

    Pais List

--- app/Models/Pais.php
+++ app/Models/Pais.php
@@ -9,6 +9,6 @@ class Pais extends Model
 {
     use HasFactory;
     protected $table = 'tb_pais';
-    protected $primaryKey = 'pais_codi';
+    protected $codigo = 'pais_codi';
     public $timestamps = false;
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ComunaController;
 use App\Http\Controllers\MunicipioController;
 use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\PaisController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -40,3 +41,5 @@ Route::get('/departamentos/create', [DepartamentoController::class,'create'])->n
 Route::delete('/departamentos/{departamento}', [departamentoController::class,'destroy'])->name('departamentos.destroy');
 Route::put('/departamentos/{departamento}', [DepartamentoController::class,'update'])->name('departamentos.update');
 Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class,'edit'])->name('departamentos.edit');
+
+Route::get('/paises', [PaisController::class,'index'])->name('paises.index');
