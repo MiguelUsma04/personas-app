2a120d70cbf8e98b42fa10156cd29258e68ca71a
Miguel Usma <usmamiguel04@gmail.com>
2023-11-01 17:36:27 -0500

    Breeze Install y Configuración de Rutas

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ComunaController;
 use App\Http\Controllers\MunicipioController;
 use App\Http\Controllers\DepartamentoController;
 use App\Http\Controllers\PaisController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::get('/comunas', [ComunaController::class,'index'])->name('comunas.index');
@@ -45,3 +56,5 @@ Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class,
 Route::get('/paises', [PaisController::class,'index'])->name('paises.index');
 Route::post('/paises', [PaisController::class,'store'])->name('paises.store');
 Route::get('/paises/create', [PaisController::class,'create'])->name('paises.create');
+
+require __DIR__.'/auth.php';
